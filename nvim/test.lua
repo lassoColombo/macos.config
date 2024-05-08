a405d121c04d6892d058e612176f3bfe1ad405b9
Simone Colombo <lasso.colombo@gmail.com>
2024-05-08 17:37:40 +0200

    removed useless test files

--- nvim/test.lua
+++
@@ -1,5 +0,0 @@
-if string.match('asd', 'asd|qwe') then
-  print 'ciao'
-  -- If there is a match
-  -- Your code here
-end
--- nvim/test.py
+++
@@ -1,7 +0,0 @@
-class className(parentClasses):
-
-    def __init__(self, asd: int, ciao=[], *args, **kargs):
-        pass
-
-    def __str__(self) -> str:
-        pass
