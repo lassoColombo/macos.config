4f8d932c93f6f49aeaa6f671d65f38263db1bf40
Simone Colombo <lasso.colombo@gmail.com>
2024-05-17 21:53:33 +0200

    deactivated some fancy stuff to prevent annoying sqls error

--- nvim/lua/custom/UI/noice.lua
+++ nvim/lua/custom/UI/noice.lua
@@ -26,12 +26,13 @@ return {
     end, { desc = '[T]oggle [N]oice' })
     return {
       lsp = {
+        -- :FIXME: disabled for terribly annoying errors in sql files. Cannot manage to disable for filetype
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-        },
+        -- override = {
+        --   ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        --   ['vim.lsp.util.stylize_markdown'] = true,
+        --   ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+        -- },
       },
       -- you can enable a preset for easier configuration
       presets = {
