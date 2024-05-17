8eedda3aa19744ca7c996c5410baaa1ef00a083e
Simone Colombo <lasso.colombo@gmail.com>
2024-05-17 21:52:52 +0200

    oil :)

---
+++ nvim/lua/custom/utils/oil.lua
@@ -0,0 +1,36 @@
+return {
+  'stevearc/oil.nvim',
+  keys = {
+    {
+      '<leader><leader>o',
+      '<cmd>Oil .<cr>',
+      mode = 'n',
+      desc = '[O]il',
+    },
+  },
+  opts = {
+    keymaps = {
+      ['g?'] = 'actions.show_help',
+      ['<CR>'] = 'actions.select',
+      ['L'] = 'actions.select',
+      ['<C-v>'] = 'actions.select_vsplit',
+      ['<C-s>'] = 'actions.select_split',
+      ['<C-t>'] = 'actions.select_tab',
+      ['<C-p>'] = 'actions.preview',
+      ['<C-c>'] = 'actions.close',
+      ['<C-n>'] = 'actions.refresh',
+      ['-'] = 'actions.parent',
+      ['<'] = 'actions.parent',
+      ['_'] = 'actions.open_cwd',
+      ['`'] = 'actions.cd',
+      ['~'] = 'actions.tcd',
+      ['gs'] = 'actions.change_sort',
+      ['gx'] = 'actions.open_external',
+      ['g.'] = 'actions.toggle_hidden',
+      ['C-h'] = 'actions.toggle_hidden',
+      ['g\\'] = 'actions.toggle_trash',
+    },
+  },
+  -- Optional dependencies
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+}
