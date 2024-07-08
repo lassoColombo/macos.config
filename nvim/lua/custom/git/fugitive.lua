28fa7635932a9ecb07ebbc27118406aea2a3dbf1
Simone Colombo <lasso.colombo@gmail.com>
2024-07-08 22:04:21 +0200

    some useful git keymaps

--- nvim/lua/custom/git/fugitive.lua
+++ nvim/lua/custom/git/fugitive.lua
@@ -4,6 +4,8 @@ return {
     { '<leader>gb', '<cmd>Git blame<cr>', desc = '[G]it [B]lame' },
     { '<leader>gL', '<cmd>Git log<cr>', desc = '[G]it [L]og' },
     { '<leader>gs', '<cmd>Git status<cr>', desc = '[G]it [S]tatus' },
+    { '<leader>gP', '<cmd>Git push<cr>', desc = '[G]it [P]ush' },
+    { '<leader>gp', '<cmd>Git pull<cr>', desc = '[G]it [P]ull' },
   },
   config = function()
     return {}
