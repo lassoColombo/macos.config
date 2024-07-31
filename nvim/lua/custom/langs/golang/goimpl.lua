0f31aaf1943de3c3b5cce4302f5f16a12609ce4f
Simone Colombo <lasso.colombo@gmail.com>
2024-07-31 19:27:17 +0200

    added goimpl

---
+++ nvim/lua/custom/langs/golang/goimpl.lua
@@ -0,0 +1,21 @@
+return {
+  'edolphin-ydf/goimpl.nvim',
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    { 'nvim-lua/popup.nvim' },
+    { 'nvim-telescope/telescope.nvim' },
+    { 'nvim-treesitter/nvim-treesitter' },
+  },
+  config = function()
+    require('telescope').load_extension 'goimpl'
+  end,
+  keys = {
+    {
+      '<leader><leader>i',
+      [[<cmd>lua require'telescope'.extensions.goimpl.goimpl{}<CR>]],
+      mode = 'n',
+      desc = '[I]mplement [I]interface',
+      ft = { 'go' },
+    },
+  },
+}
